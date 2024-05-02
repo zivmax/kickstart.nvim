ceac55f94fa5a45c5e7e56ec1c629f76a58c370b
zivmax <zivmax@foxmail.com>
2024-05-02 10:56:51 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -598,7 +598,7 @@ require('lazy').setup({
           },
         },
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
